9babebe4a5e90e01eff80050c44e4ebabfccadd5
chenxi <jsjliyang@gmail.com>
2019-08-01 09:56:02 +0800

    java_jna_c

---
+++ JAN/Jantest.java
@@ -0,0 +1,13 @@
+package JAN;
+import com.sun.jna.*;
+public class Jantest {
+    public interface TestDll1 extends Library {
+        TestDll1 INSTANCE = (TestDll1)Native.load("D:\\Desktop\\javacode\\javacoding\\JAN\\DLL_ly.dll",TestDll1.class);
+        public int say(int a, int b);
+
+    }
+    public static void main(String[] args) {
+        int i = TestDll1.INSTANCE.say(12,13);
+        System.out.println("Java代码 " + i);
+    }
+}
