5218214fd2b69f67f52217bbc946cb126e9a7933
chenxi <jsjliyang@gmail.com>
2019-08-13 17:35:27 +0800

    8.13

---
+++ c11test13.java
@@ -0,0 +1,17 @@
+// 第11章 练习12
+// @author 晴和羽织
+import java.util.*;
+import static lyprint.Print.*;
+public class c11test13 {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        ListIterator<Integer> it = list.listIterator();
+        for(int i = 0; i < 11; i++) {
+            it.add(i);
+            if(i % 2 == 0 && i != 0)
+                it.previous();
+            printl("[" +  i + "]: " + list);
+        }
+        //printl(list);
+    }
+}
